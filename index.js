e47dfff70b6113539faec4bea6963aa862237321
ViolettaParamonova <violettapar1995@gmail.com>
2022-09-14 12:26:40 +0300

    uploads

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import fs from 'fs';
 import multer from 'multer';
 import cors from 'cors';
 import mongoose from 'mongoose';
@@ -16,6 +17,9 @@ const app = express();
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
+    if (!fs.existsSync('uploads')) {
+      fs.mkdtempSync('uploads')
+    }
     cb(null, 'uploads');
   },
   filename: (_, file, cb) => {
